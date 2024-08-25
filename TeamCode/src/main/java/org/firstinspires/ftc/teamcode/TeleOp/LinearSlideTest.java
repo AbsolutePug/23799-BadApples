eda2383c51ffacf0af6b7f5d585084e9841fee80
AbsolutePug <100257223+AbsolutePug@users.noreply.github.com>
2024-08-25 16:34:43 -0500

    Changes to LinearSlideTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LinearSlideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LinearSlideTest.java
@@ -33,21 +33,12 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 /*
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive TeleOp for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ This code tests a linear slide cycling
  */
 
 @TeleOp(name="Linear Slide Test", group="Test")
@@ -55,7 +46,11 @@ public class LinearSlideTest extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
+    ElapsedTime slide_progress = new ElapsedTime();
     private DcMotor motor = null;
+    boolean slide_target = false;
+    double max_length = 1;
+    boolean slide_finished = true;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -80,30 +75,30 @@ public class LinearSlideTest extends OpMode
      //Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
     @Override
     public void loop() {
-        boolean forward = gamepad1.dpad_up;
-        boolean reverse = gamepad1.dpad_down;
-
-        double power;
-
-        if (forward) {
-            power = 0.3;
-        } else if (reverse) {
-            power = -0.3;
-        } else {
-            power = 0;
+        motor.setDirection(DcMotor.Direction.REVERSE);
+        if (slide_progress.seconds() >= max_length) {
+            slide_finished = true;
         }
 
-        boolean a = gamepad1.a;
-        if (a) {
-            runtime.reset();
+        // Slide state change
+        if (gamepad1.right_bumper && slide_finished) {
+            slide_progress.reset();
+            slide_finished = false;
+            slide_target = !slide_target; // Toggle
         }
 
-        motor.setPower(power);
-
+        // Move slide
+        if (slide_finished) {motor.setPower(0);} // The slide is in its target position
+        else {
+            if (slide_target) {motor.setPower(1);}
+            else {motor.setPower(-1);}
+        }
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors speed", power);
-        telemetry.addData("", power);
+        telemetry.addData("Status", "Run Time(s): " + runtime.seconds());
+        telemetry.addData("Current", slide_target);
+        telemetry.addData("Result", slide_finished);
+        telemetry.addData("Progress", slide_progress.toString());
         telemetry.update();
     }
 }
