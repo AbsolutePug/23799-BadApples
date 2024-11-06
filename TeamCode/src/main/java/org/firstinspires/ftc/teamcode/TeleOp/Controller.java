149ea13f62d8d914759e450f1a74f46e2edc828c
AbsolutePug <100257223+AbsolutePug@users.noreply.github.com>
2024-11-06 00:15:54 -0600

    Changes to TeamCode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Controller.java
@@ -5,15 +5,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 /*
-Written by Robert Maddox (2024) @AbsolutePug (github.com/AbsolutePug)
+    Written by Robert Maddox (2024) @AbsolutePug (github.com/AbsolutePug)
 */
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TeleOp", group="Custom")
-public class TeleOp extends LinearOpMode {
+public class Controller extends LinearOpMode {
     // Declare OpMode members for each of the 4 motors.
     private final ElapsedTime runtime = new ElapsedTime();
     private final ElapsedTime loop_time = new ElapsedTime();
-    private final ElapsedTime arm_timeout = new ElapsedTime();
     private DcMotor leftFront = null;
     private DcMotor leftBack = null;
     private DcMotor rightFront = null;
@@ -21,6 +20,16 @@ public class TeleOp extends LinearOpMode {
     private DcMotor arm = null;
     private Servo wrist = null;
     private CRServo claw = null;
+
+    // Constants
+    final double arm_x_right = 0.18;
+    final double arm_x_center = 0.5;
+    final double arm_x_left = 0.82;
+
+    // Variables
+    boolean arm_active = false; // Controls whether arm can be controlled by user input
+    boolean arm_busy = false; // Is the arm currently changing state?
+    double wrist_x = arm_x_left;
 
     // Inputs
     public boolean gamepad_a = false;
@@ -34,7 +43,7 @@ public class TeleOp extends LinearOpMode {
         gamepad_a = gamepad1.a;
 
         // Input
-        Input_brake = gamepad_a && gamepad_a_last;
+        Input_brake = gamepad_a && !gamepad_a_last;
         if (gamepad2.right_bumper && !gamepad_right_bumper_last) {
             if (Input_claw == 1) {Input_claw = 0;}
             else                 {Input_claw = 1;}
@@ -48,15 +57,67 @@ public class TeleOp extends LinearOpMode {
         gamepad_a_last = gamepad_a;
         gamepad_left_bumper_last  = gamepad2.left_bumper;
         gamepad_right_bumper_last = gamepad2.right_bumper;
-    }
+    } // Method to update inputs
     void armDeploy() {
+        ElapsedTime deploy_timeout = new ElapsedTime();
+        deploy_timeout.reset();
+        arm_active = false;
+        arm_busy = true;
 
+        while (deploy_timeout.milliseconds() < 1000) {
+            // Beginning
+            if (deploy_timeout.milliseconds() < 500) {
+                arm.setPower(1);
+            } // Move up for first 500ms
+            else {
+                arm.setPower(-1);
+            } // Move down for 500ms after 500ms
+            // Servo
+            if (deploy_timeout.milliseconds() > 500)  {
+                wrist.setPosition(arm_x_center);
+                wrist_x = arm_x_center;
+                arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            } // Move servo after moving arm up
+        }
+        // Arm has finished changing state
+        arm_active = true;
+        arm_busy = false;
+    }
+    void dtSetPower(double FL, double FR, double BL, double BR) {
+        leftFront.setPower(-FL);
+        rightFront.setPower(-FR);
+        leftBack.setPower(-BL);
+        rightBack.setPower(-BR);
+    } // "dt" : Drivetrain
+    void armSet(double Power) {
+
+    }
+    void autoHang() {
+        ElapsedTime hang_timeout = new ElapsedTime();
+        hang_timeout.reset();
+        arm_active = false;
+        arm_busy = true;
+
+        while (opModeIsActive()) {
+            // Drive
+            if (hang_timeout.milliseconds() > 1500 && hang_timeout.milliseconds() < 2500) {
+                dtSetPower(0.5,0.5,0.5,0.5);
+            } else {
+                dtSetPower(0,0,0,0);
+            }
+
+            // Arm
+            if (hang_timeout.milliseconds() < 1600) {
+                arm.setPower(1);
+            } else {
+                arm.setPower(-1);
+            }
+        }
     }
 
     @Override
     public void runOpMode() {
-        // Initialize the hardware variables. Note that the strings used here must correspond
-        // to the names assigned during the robot configuration step on the DS or RC devices.
+        // Initialize the hardware variables.
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
@@ -70,30 +131,22 @@ public class TeleOp extends LinearOpMode {
         leftBack.setDirection(DcMotor.Direction.REVERSE);
         rightFront.setDirection(DcMotor.Direction.FORWARD);
         rightBack.setDirection(DcMotor.Direction.FORWARD);
+        arm.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Init, Ready for Start!");
         telemetry.update();
-
         waitForStart();
         runtime.reset();
 
-        //Set brake to true initially
+        // Set zero power behaviors
         boolean brake = true;
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-        // Variables
-        boolean arm_active = false;
-        boolean arm_state_change = false; // Is the arm currently changing state?
-        boolean active = false;
-        final double arm_x_right = 0.18;
-        final double arm_x_center = 0.5;
-        final double arm_x_left = 0.82;
-        double wrist_x = arm_x_left;
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        wrist.setPosition(arm_x_left);
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
@@ -109,49 +162,24 @@ public class TeleOp extends LinearOpMode {
             double arm_y   = -gamepad2.right_stick_y; // Up/Down
 
             // Scoring device
-            if (!arm_state_change) {
-                if (arm_active) {
-                    arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                    arm.setPower(arm_y);
-                    // Wrist Control
-                    if (gamepad2.dpad_left) {wrist_x = arm_x_left;}
-                    else if (gamepad2.dpad_right) {wrist_x = arm_x_right;}
-                    else if (gamepad2.dpad_up || gamepad2.dpad_down) {wrist_x = arm_x_center;}
-                    wrist.setPosition(wrist_x);
+            if (arm_active) {
+                arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                arm.setPower(arm_y);
+                // Wrist Control
+                if (gamepad2.dpad_left) {wrist_x = arm_x_left;}
+                else if (gamepad2.dpad_right) {wrist_x = arm_x_right;}
+                else if (gamepad2.dpad_up || gamepad2.dpad_down) {wrist_x = arm_x_center;}
+                wrist.setPosition(wrist_x);
 
-                    // Claw control
-                    claw.setPower(Input_claw);
-                }
-                else {
-                    if (gamepad2.b) {
-                        arm_state_change = true;
-                        arm_timeout.reset();
-                    } // Press to change state
-                    arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-                    arm.setPower(0);
-                    wrist.setPosition(arm_x_right);
-                    claw.setPower(0);
-                }
-            } // Only allow control of arm if it is not changing state
-            // Arm is currently changing states
-            if (arm_state_change) {
-                // Beginning
-                if (arm_timeout.milliseconds() < 500) {arm.setPower(1);}
-                else                                   {arm.setPower(-1);} // Arm Y Movement
-                //
-                if (arm_timeout.milliseconds() > 500)  {
-                    if (arm_active) {wrist.setPosition(arm_x_right);} // Changing from active to inactive
-                    else            {wrist.setPosition(arm_x_center);
-                                     wrist_x = arm_x_center;       } // Changing from inactive to active
-                    arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                } // Move servo after moving arm up
-                //
-                if (arm_timeout.milliseconds() > 1000) {
-                    arm_active = !arm_active;
-                    arm_state_change = false;
-                } // Arm has finished changing state
-            } // Arm changing
+                // Claw control
+                claw.setPower(Input_claw);
 
+                // Hang
+                if (gamepad2.y && !arm_busy) {autoHang();}
+            }
+            else {
+                if (gamepad2.b && !arm_busy) {armDeploy();} // Press to deploy arm
+            }
 
             // Accuracy mode. If right stick is more than half way pressed. Enable Accuracy mode (slow-mode)
             boolean accuracy_mode = trigger > .5;
@@ -203,8 +231,8 @@ public class TeleOp extends LinearOpMode {
             rightBack.setPower(rightBackPower);
 
             // Basic Telemetry
-            telemetry.addData("Status", "" + runtime);
-            telemetry.addData("Loop time", loop_time.toString());
+            telemetry.addData("Status", runtime.toString());
+            telemetry.addData("Loop time (ms)", loop_time.milliseconds());
             // Brake Telemetry
             if (brake)  {telemetry.addData("Brake", "Engaged");}
             else        {telemetry.addData("Brake", "Disengaged");}
@@ -225,7 +253,6 @@ public class TeleOp extends LinearOpMode {
             telemetry.update();
 
             loop_time.reset();
-
         }
     }
 }
